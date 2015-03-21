da7889a517f11f8af29be0fcf86b61b350cf1eb8
juliocvidal <juliocvidal@gmail.com>
2015-03-21 17:48:39 -0300

    atualizacao do form de pagamento

---
+++ html-final/assets/js/project.js
@@ -0,0 +1,42 @@
+
+
+$(document).ready(function() { // quando o jQuery estiver carregado...
+    PagarMe.encryption_key = "ek_test_ZlmbSDu9eYegBIp9YrpDN7B0I7saEW";
+
+    var paymentButton = $("#payment_button");
+
+    function pay() { // quando o form for enviado...
+        // inicializa um objeto de cartão de crédito e completa
+        // com os dados do form
+        alert('Funfou o js!');
+        console.log('funfou!');
+        var creditCard = new PagarMe.creditCard();
+        creditCard.cardHolderName = $("#payment_form #card_holder_name").val();
+        creditCard.cardExpirationMonth = $("#payment_form #card_expiration_month").val();
+        creditCard.cardExpirationYear = $("#payment_form #card_expiration_year").val();
+        creditCard.cardNumber = $("#payment_form #card_number").val();
+        creditCard.cardCVV = $("#payment_form #card_cvv").val();
+
+        // pega os erros de validação nos campos do form
+        var fieldErrors = creditCard.fieldErrors();
+
+        //Verifica se há erros
+        var hasErrors = false;
+        for(var field in fieldErrors) { hasErrors = true; break; }
+
+        if(hasErrors) {
+            // realiza o tratamento de errors
+            alert(fieldErrors);
+        } else {
+            // se não há erros, gera o card_hash...
+            creditCard.generateHash(function(cardHash) {
+                // ...coloca-o no form...
+                form.append($('<input type="hidden" name="card_hash">').val(cardHash));
+                // e envia o form
+                form.get(0).submit();
+            });
+        }
+
+        return false;
+    }
+});
